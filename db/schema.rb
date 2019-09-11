5888b33471c2d5d5db28ea3d69a9950127ebcb46
Cameron Panlasigui <cameron.panlasigui@appfolio.com>
2019-09-11 09:56:24 -0700

    CP - create image table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_09_10_174523) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+  end
 
 end
