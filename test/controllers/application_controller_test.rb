a6d4b8c415800779e2ed974535a47778328d28dd
Cameron Panlasigui <cameron.panlasigui@appfolio.com>
2019-09-11 09:58:17 -0700

    CP - Add image controller with new and show actions

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  test 'should get home' do
-    get root_path
-    assert_select 'body', 'Hello'
-    assert_response :success
-  end
 end
