c138b2480a178816b82b1a69d7aab96603be367f
Cameron Panlasigui <cameron.panlasigui@appfolio.com>
2019-09-11 09:56:39 -0700

    CP - Add image model

---
+++ test/models/image_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class ImageTest < ActiveSupport::TestCase
+  test 'is valid image' do
+    image = Image.new(url: 'https://image.jpg')
+    assert_predicate image, :valid?
+  end
+
+  test 'invalid with bad url' do
+    image = Image.new(url: 'image.jpg')
+    refute_predicate image, :valid?
+    assert_equal ['Not an image url'], image.errors.messages[:url]
+
+    image = Image.new(url: 'https://image')
+    refute_predicate image, :valid?
+    assert_equal ['Not an image url'], image.errors.messages[:url]
+  end
+end
