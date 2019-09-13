62fd966d335e00828dcea5bfe9d35135eee78b59
Cameron Panlasigui <cameron.panlasigui@appfolio.com>
2019-09-13 12:35:39 -0700

    CP - Add ability to submit with status messages

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -1,23 +1,87 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React, { useState } from 'react';
+import { Button, Form, FormGroup, Label, Input, Alert } from 'reactstrap';
+import { serialize, post } from '../utils/helper';
 
-const formStyle = {
-  maxWidth: '40%',
-  margin: '0 auto'
+const styles = {
+  formStyle: {
+    maxWidth: '40%',
+    margin: '0 auto'
+  },
+  inlineStyle: {
+    display: 'inline'
+  },
+  leftMarginStyle: {
+    marginLeft: '20px'
+  }
 };
 
 export default function FeedbackForm() {
+  const [alert, setAlert] = useState();
+  const [name, setName] = useState('');
+  const [comment, setComment] = useState('');
+
+  function clearInputs() {
+    setName('');
+    setComment('');
+  }
+
+  function displayAlert(status, message) {
+    setAlert({
+      color: status,
+      message
+    });
+  }
+
+  function handleClick() {
+    const queryParams = serialize({
+      feedback: {
+        name,
+        comment
+      }
+    });
+
+    return post(`/api/feedbacks?${queryParams}`)
+      .then((response) => {
+        displayAlert('success', response.message || 'Success');
+        clearInputs();
+      })
+      .catch(() => {
+        displayAlert('warning', 'Name and Comments Required');
+      });
+  }
+
   return (
-    <div style={formStyle} >
-      <Form>
+    <div style={styles.formStyle} >
+      <Form className='feedback-form'>
         <FormGroup>
           <Label for='nameInput'>Your name:</Label>
-          <Input type='text' id='nameInput' />
+          <Input onChange={e => setName(e.target.value)} type='text' value={name} id='nameInput' />
           <Label for='commentsInput'>Comments:</Label>
-          <Input type='text' id='commentsInput' />
+          <Input
+            onChange={e => setComment(e.target.value)}
+            type='text'
+            value={comment}
+            id='nameInput'
+          />
         </FormGroup>
       </Form>
-      <Button color="primary">Submit</Button>
+      <div>
+        <Button
+          style={styles.inlineStyle}
+          onClick={() => handleClick()}
+          color="primary"
+        >
+          Submit
+        </Button>
+        {alert &&
+          <Alert
+            style={{ ...styles.inlineStyle, ...styles.leftMarginStyle }}
+            color={alert.color}
+          >
+            {alert.message}
+          </Alert>
+        }
+      </div>
     </div>
   );
 }
--- app/javascript/image_sharing/test/components/FeedbackForm.spec.js
+++ app/javascript/image_sharing/test/components/FeedbackForm.spec.js
@@ -3,7 +3,9 @@
 import assert from 'assert';
 import { shallow } from 'enzyme';
 import React from 'react';
+import sinon from 'sinon';
 import FeedbackForm from '../../components/FeedbackForm';
+import * as api from '../../utils/helper';
 
 describe('<FeedbackForm />', () => {
   it('should render correctly', () => {
@@ -19,5 +21,54 @@ describe('<FeedbackForm />', () => {
     assert.strictEqual(labels.first().children().text(), 'Your name:');
     assert.strictEqual(labels.last().children().text(), 'Comments:');
     assert.strictEqual(buttons.children().text(), 'Submit');
+  });
+
+  it('should clear form and render success alert when request succeeds', async () => {
+    const wrapper = shallow(<FeedbackForm />);
+    let inputs = wrapper.find('Input');
+
+    inputs.at(0).simulate('change', { target: { value: 'Name' } });
+    inputs.at(1).simulate('change', { target: { value: 'Comment' } });
+
+    wrapper.update();
+    inputs = wrapper.find('Input');
+    const button = wrapper.find('Button');
+
+    assert.strictEqual(inputs.at(0).prop('value'), 'Name');
+    assert.strictEqual(inputs.at(1).prop('value'), 'Comment');
+
+    const postStub = sinon.stub(api, 'post').resolves({});
+    const goodRequest =
+      '/api/feedbacks?feedback%5Bname%5D=Name&feedback%5Bcomment%5D=Comment';
+    await button.prop('onClick')();
+    wrapper.update();
+    inputs = wrapper.find('Input');
+    const alert = wrapper.find('Alert');
+
+    assert(postStub.calledWith(goodRequest));
+
+    assert.strictEqual(inputs.at(0).prop('value'), '');
+    assert.strictEqual(inputs.at(1).prop('value'), '');
+    assert.strictEqual(alert.prop('color'), 'success');
+    assert.strictEqual(alert.children().text(), 'Success');
+
+    sinon.restore();
+  });
+
+  it('should render alert message when request is bad', async () => {
+    const wrapper = shallow(<FeedbackForm />);
+    const button = wrapper.find('Button');
+
+    const postStub = sinon.stub(api, 'post').rejects({});
+
+    await button.prop('onClick')();
+    wrapper.update();
+    const alert = wrapper.find('Alert');
+
+    assert(postStub.calledOnce);
+    assert.strictEqual(alert.prop('color'), 'warning');
+    assert.strictEqual(alert.children().text(), 'Name and Comments Required');
+
+    sinon.restore();
   });
 });
